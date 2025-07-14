674ed682d4928962887790726d1f55360c09a2e9
Ruy Vieira <ruy.vieiraneto@gmail.com>
2025-07-14 01:20:37 -0300

    feature: error handling at contact media

--- src/app/contacts/contact-media/contact-media.component.ts
+++ src/app/contacts/contact-media/contact-media.component.ts
@@ -1,17 +1,16 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, ViewChild } from "@angular/core";
 import { SmallButtonComponent } from "../../common/small-button/small-button.component";
 import { MediaPreviewComponent } from "../contact-info/media-preview/media-preview.component";
 import { ActivatedRoute, Router, RouterModule } from "@angular/router";
-import {
-    Conversation,
-    ConversationMessagingProductContact,
-} from "../../../core/message/model/conversation.model";
+import { Conversation, ConversationMessagingProductContact } from "../../../core/message/model/conversation.model";
 import { CommonModule } from "@angular/common";
 import { MediaMode } from "./enum/media-mode.enum";
 import { ConversationControllerService } from "../../../core/message/controller/conversation-controller.service";
 import { DateOrderEnum } from "../../../core/common/model/date-order.model";
 import { CapitalizeFirstLetterPipe } from "../../../core/common/pipe/capitalize-first-letter.pipe";
 import { QueryParamsService } from "../../../core/navigation/service/query-params.service";
+import { NGXLogger } from "ngx-logger";
+import { TimeoutErrorModalComponent } from "../../common/timeout-error-modal/timeout-error-modal.component";
 
 @Component({
     selector: "app-contact-media",
@@ -21,6 +20,7 @@ import { QueryParamsService } from "../../../core/navigation/service/query-param
         MediaPreviewComponent,
         CapitalizeFirstLetterPipe,
         RouterModule,
+        TimeoutErrorModalComponent,
     ],
     templateUrl: "./contact-media.component.html",
     styleUrl: "./contact-media.component.scss",
@@ -37,21 +37,18 @@ export class ContactMediaComponent implements OnInit {
 
     // Pagination parameters and media content
     mediaLimit: number = 40;
-    media = new Map<MediaMode, Conversation[]>(
-        Object.values(MediaMode).map((value) => [value, []]),
-    ); // Media content
-    scrolling = new Map<MediaMode, boolean>(
-        Object.values(MediaMode).map((value) => [value, false]),
-    ); // Scrolling status
-    reachedMaxMediaLimit = new Map<MediaMode, boolean>(
-        Object.values(MediaMode).map((value) => [value, false]),
-    );
+    media = new Map<MediaMode, Conversation[]>(Object.values(MediaMode).map((value) => [value, []])); // Media content
+    scrolling = new Map<MediaMode, boolean>(Object.values(MediaMode).map((value) => [value, false])); // Scrolling status
+    reachedMaxMediaLimit = new Map<MediaMode, boolean>(Object.values(MediaMode).map((value) => [value, false]));
+
+    @ViewChild("errorModal") errorModal!: TimeoutErrorModalComponent;
 
     constructor(
         private router: Router,
         private route: ActivatedRoute,
         private conversationController: ConversationControllerService,
         private queryParamsService: QueryParamsService,
+        private logger: NGXLogger,
     ) {}
 
     ngOnInit(): void {
@@ -61,9 +58,9 @@ export class ContactMediaComponent implements OnInit {
     async getMedia(mode: MediaMode) {
         this.scrolling.set(mode, true);
 
-        // New media
-        const newMedia =
-            await this.conversationController.conversationContentLike(
+        try {
+            // New media
+            const newMedia = await this.conversationController.conversationContentLike(
                 this.messagingProductContact.id,
                 `"type"\\s*:\\s*"(${mode})"`,
                 undefined,
@@ -73,23 +70,24 @@ export class ContactMediaComponent implements OnInit {
                 },
                 { created_at: DateOrderEnum.desc },
             );
-        if (newMedia.length === 0) {
-            this.reachedMaxMediaLimit.set(mode, true);
+            if (newMedia.length === 0) {
+                this.reachedMaxMediaLimit.set(mode, true);
+                this.scrolling.set(mode, false);
+                return;
+            }
+            this.media.get(mode)?.push(...newMedia);
+        } catch (error) {
+            this.handleErr("Error getting media", error);
+        } finally {
             this.scrolling.set(mode, false);
-            return;
         }
-        this.media.get(mode)?.push(...newMedia);
-
-        this.scrolling.set(mode, false);
-        return;
     }
 
     onScroll(event: Event, mode: MediaMode) {
         const element = event.target as HTMLElement;
 
         if (
-            element.scrollHeight - element.scrollTop <=
-                element.clientHeight + 100 &&
+            element.scrollHeight - element.scrollTop <= element.clientHeight + 100 &&
             !this.scrolling.get(mode) &&
             !this.reachedMaxMediaLimit.get(mode)
         )
@@ -110,8 +108,7 @@ export class ContactMediaComponent implements OnInit {
 
     watchQueryParams() {
         this.route.queryParams.subscribe(async (params) => {
-            const messagingProductContactId =
-                params["messaging_product_contact.id"];
+            const messagingProductContactId = params["messaging_product_contact.id"];
             if (
                 !messagingProductContactId ||
                 messagingProductContactId != this.messagingProductContact.id ||
@@ -120,17 +117,22 @@ export class ContactMediaComponent implements OnInit {
                 return;
             const currentModeKey = params["contact_media.mode"];
             if (!currentModeKey) return this.selectMediaMode(MediaMode.image);
-            const currentModeKeyConverted =
-                currentModeKey as keyof typeof MediaMode;
+            const currentModeKeyConverted = currentModeKey as keyof typeof MediaMode;
             const currentMode = MediaMode[currentModeKeyConverted];
             this.currentMediaMode = currentMode;
 
-            if (
-                !this.scrolling.get(currentMode) &&
-                !this.reachedMaxMediaLimit.get(currentMode)
-            )
+            if (!this.scrolling.get(currentMode) && !this.reachedMaxMediaLimit.get(currentMode))
                 await this.getMedia(currentMode);
             return;
         });
+    }
+
+    errorStr: string = "";
+    errorData: any;
+    handleErr(message: string, err: any) {
+        this.errorData = err?.response?.data;
+        this.errorStr = err?.response?.data?.description || message;
+        this.logger.error("Async error", err);
+        this.errorModal.openModal();
     }
 }
